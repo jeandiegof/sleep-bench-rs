cf78f5f028c6b14eb9b305d1d129bc956ddfa480
Jean Fontena <diegofontena@gmail.com>
2022-06-07 14:58:04 +0200

    Measure energy and time

--- src/bench_record.rs
+++ src/bench_record.rs
@@ -1,16 +1,32 @@
-use pinscher::BenchResult;
+use pinscher::{CpuTimeBencher, EnergyBencher};
 use serde::Serialize;
 
 #[derive(Serialize, Debug)]
 pub struct BenchRecord {
+    /// d parameter of the algorithm
     d: u64,
+
+    /// CPU active time in micro seconds
     cpu_time_us: u128,
+
+    /// Package energy in micro Joules
+    package_energy: u64,
+
+    /// Core energy in micro Joules
+    core_energy: u64,
 }
 
 impl BenchRecord {
-    pub fn new(d: u64, bench_result: BenchResult) -> Self {
-        let cpu_time_us = bench_result.cpu_time().as_micros();
+    pub fn new(d: u64, cpu_time: CpuTimeBencher, energy: EnergyBencher) -> Self {
+        let cpu_time_us = cpu_time.cpu_time().unwrap().as_micros();
+        let package_energy = energy.package_energy();
+        let core_energy = energy.core_energy();
 
-        Self { d, cpu_time_us }
+        Self {
+            d,
+            cpu_time_us,
+            package_energy,
+            core_energy,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod bench_record;
 use app_args::AppArgs;
 use bench_record::BenchRecord;
 use csv::Writer;
-use pinscher::Bencher;
+use pinscher::{BenchSuite, CpuTimeBencher, EnergyBencher};
 use std::{thread, time::Duration};
 
 fn sleep(t: u64, d: u64) {
@@ -17,10 +17,16 @@ fn main() {
     let args = AppArgs::new();
     let mut csv_writer = Writer::from_path(args.output_filename()).unwrap();
 
-    for d in 1..args.d_max() {
-        let bench_result = Bencher::bench(|| sleep(args.t(), d));
-        let record = BenchRecord::new(d, bench_result);
-        println!("{:?}", record);
+    for d in 1..=args.d_max() {
+        println!("Running {}/{}", d, args.d_max());
+
+        let mut cpu_time_bencher = CpuTimeBencher::new();
+        BenchSuite::bench(|| sleep(args.t(), d), &mut cpu_time_bencher).unwrap();
+
+        let mut energy_bencher = EnergyBencher::new().unwrap();
+        BenchSuite::bench(|| sleep(args.t(), d), &mut energy_bencher).unwrap();
+
+        let record = BenchRecord::new(d, cpu_time_bencher, energy_bencher);
 
         csv_writer.serialize(record).unwrap();
         csv_writer.flush().unwrap();
