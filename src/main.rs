7eeabaed251f5684cabffa2e610370adf82f2291
Jean Fontena <diegofontena@gmail.com>
2022-06-14 10:26:59 +0200

    Implement Benchable for Sleep

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,29 @@ mod bench_record;
 use app_args::AppArgs;
 use bench_record::BenchRecord;
 use csv::Writer;
-use pinscher::{BenchSuite, CpuTimeBencher, EnergyBencher};
+use pinscher::{BenchSuite, Benchable, CpuTimeBencher, EnergyBencher};
 use std::{thread, time::Duration};
 
-fn sleep(t: u64, d: u64) {
-    for _ in 0..(t / d) {
-        thread::sleep(Duration::from_micros(d));
+struct Sleep {
+    t: u64,
+    d: u64,
+}
+
+impl Sleep {
+    pub fn new(t: u64, d: u64) -> Self {
+        Self { t, d }
+    }
+}
+
+impl Benchable for Sleep {
+    fn name(&self) -> &'static str {
+        "Sleep t/d"
+    }
+
+    fn execute(&mut self) {
+        for _ in 0..(self.t / self.d) {
+            thread::sleep(Duration::from_micros(self.d));
+        }
     }
 }
 
@@ -21,12 +38,13 @@ fn main() {
         println!("Run {}/{}", r, args.runs());
         (1..=args.d_max()).for_each(|d| {
             println!("d = {}/{}, t = {}", d, args.d_max(), args.t());
+            let mut sleep = Sleep::new(args.t(), d);
 
             let mut cpu_time_bencher = CpuTimeBencher::new();
-            BenchSuite::bench(|| sleep(args.t(), d), &mut cpu_time_bencher).unwrap();
+            BenchSuite::bench(&mut sleep, &mut cpu_time_bencher).unwrap();
 
             let mut energy_bencher = EnergyBencher::new().unwrap();
-            BenchSuite::bench(|| sleep(args.t(), d), &mut energy_bencher).unwrap();
+            BenchSuite::bench(&mut sleep, &mut energy_bencher).unwrap();
 
             let record = BenchRecord::new(d, cpu_time_bencher, energy_bencher);
             csv_writer.serialize(record).unwrap();
